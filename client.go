61da7918da5b7be76608b13fc1430a87e1f9dcd5
jesus <jsangama@pucp.edu.pe>
2022-01-08 21:54:19 -0500

    Path flag is no longer optional for receive mode

--- client.go
+++ client.go
@@ -11,23 +11,24 @@ import (
 
 //Constantes
 const NUMBER_OF_CHANNELS = 8   //Cantidad de canales disponibles para que un cliente se suscriba
+const BUFFER_SIZE = 1024       //Tamaño de un buffer temporal utilizado para leer archivos iterativamente
 const SERVER_PORT = "7101"     //Puerto en el que opera el servidor
 const FILENAME_MAX_LENGTH = 40 //Tamaño máximo del nombre de un archivo que se recibe
-const DEFAULT_DOWNLOAD_PATH = "D:\\Libraries\\Documentos\\testClient\\"
 
 func main() {
 	//Verificar argumentos
-	if len(os.Args) < 4 || len(os.Args) > 6 {
+	if len(os.Args) < 5 || len(os.Args) > 6 {
 		fmt.Println("File sharing client: Send and receive files using channels through a TCP server\n")
 		fmt.Println("Usage:")
-		fmt.Println("Receive mode:\t client receive -channel CHANNEL [-path DOWNLOAD_PATH]")
+		fmt.Println("Receive mode:\t client receive -channel CHANNEL -path DOWNLOAD_PATH")
 		fmt.Println("Send mode:\t client send FILE -channel CHANNEL")
 		fmt.Println("\nExamples:")
-		fmt.Println("client receive -channel 1 //Receive files sent by other clients to channel 1 using the default download path")
-		fmt.Println("client receive -channel 3 -path D:\\Downloads\\ //Receive files sent by other clients to channel 3 using a custom download path")
+		fmt.Println("client receive -channel 3 -path D:\\Downloads\\ //Receive files sent by other clients to channel 3, saving them to selected download path")
 		fmt.Println("client send test.txt -channel 4 //Send file test.txt to clients currently subscribed to channel 4")
 		os.Exit(0)
 	}
+	//Verificar flags
+	validateFlags()
 
 	var mode string = os.Args[1]
 	var channel int8
@@ -37,12 +38,7 @@ func main() {
 	case "receive":
 		//Leer canal y, opcionalmente, path de descarga de archivos
 		channel = parseChannel(os.Args[3])
-		var downloadPath string
-		if len(os.Args) == 6 {
-			downloadPath = parseDownloadPath(os.Args[5])
-		} else {
-			downloadPath = DEFAULT_DOWNLOAD_PATH
-		}
+		var downloadPath string = parseDownloadPath(os.Args[5])
 
 		subscribeToChannel(channel, downloadPath)
 	case "send":
@@ -54,6 +50,27 @@ func main() {
 	default:
 		fmt.Println("ERROR: Invalid command \"" + mode + "\"")
 		os.Exit(1)
+	}
+}
+
+func validateFlags() {
+	switch len(os.Args) {
+	case 5:
+		//Only validate channel flag
+		if os.Args[3] != "-channel" {
+			fmt.Println("ERROR: Incorrect flag (expected \"-channel\", got \"" + os.Args[3] + "\")")
+			os.Exit(1)
+		}
+	case 6:
+		//Validate both channel and path flags
+		if os.Args[2] != "-channel" {
+			fmt.Println("ERROR: Incorrect flag (expected \"-channel\", got \"" + os.Args[2] + "\")")
+			os.Exit(1)
+		}
+		if os.Args[4] != "-path" {
+			fmt.Println("ERROR: Incorrect flag (expected \"-path\", got \"" + os.Args[4] + "\")")
+			os.Exit(1)
+		}
 	}
 }
 
@@ -77,6 +94,12 @@ func parseDownloadPath(path string) string {
 	//Revisar que el path recibido contenga un separador de directorio al final
 	if !strings.HasSuffix(path, string(os.PathSeparator)) {
 		path += string(os.PathSeparator)
+	}
+	//Validar que el path sea válido (básicamente, que exista el directorio
+	_, err := os.Stat(path[:len(path)-1])
+	if os.IsNotExist(err) {
+		fmt.Println("ERROR: Invalid path")
+		os.Exit(1)
 	}
 	return path
 }
